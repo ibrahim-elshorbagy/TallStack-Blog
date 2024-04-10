8094525023effd623624fb0ec24c02a9103ec8bf
ibrahim-elshorbagy <ibrahim.moh0007@gmail.com>
2024-04-10 21:30:14 +0200

    before postlist livewire

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,13 +1,14 @@
-<?php
+<?php 
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        return view('posts.index',['posts'=>Post::take(5)->latest()->get()]);
     }
 }
